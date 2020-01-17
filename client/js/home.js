1cbaf47362566333a8000005feb57fe421a27b7e
RAJ000000 <59118025+RAJ000000@users.noreply.github.com>
2020-01-17 18:48:49 +0300

    Add files via upload

---
+++ client/js/home.js
@@ -0,0 +1,7 @@
+function popUp(){
+    swal( "done!", "Thanks for asking the Question!", "success");
+}
+
+function popUp2(){
+    swal("Good job!", "You answered the Question!!", "success");
+}
---
+++ client/js/qqq.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("please select a question or an answer to vote for {like or dislike}");
+  }
